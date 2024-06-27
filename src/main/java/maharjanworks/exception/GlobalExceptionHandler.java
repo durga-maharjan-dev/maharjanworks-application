a7bc3f66a5bf1d53eb524265d9dc6262218e8c18
durga.maharjan.dev <durga.maharjan.dev@gmail.com>
2024-06-27 18:16:22 -0400

    GlobalExceptionHandler is updated

--- src/main/java/maharjanworks/exception/GlobalExceptionHandler.java
+++ src/main/java/maharjanworks/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package maharjanworks.exception;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,10 +12,46 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	
-	@ExceptionHandler(EmailAlreadyExistsException.class)
+	@ExceptionHandler(value = {EmailAlreadyExistsException.class})
 	public ResponseEntity<?> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex, WebRequest webRequest){
-		ApplicationStatus applicationStatus = new ApplicationStatus(ex.getMessage(),webRequest.getDescription(true));
-		return new ResponseEntity<>(applicationStatus, HttpStatus.FOUND);
+		
+		HttpStatus httpStatus = HttpStatus.FOUND;
+		
+		ApplicationStatus applicationStatus = new ApplicationStatus(
+				ex.getMessage(),
+				ex.getCause(), 
+				httpStatus,
+				ZonedDateTime.now(ZoneId.of("Z")),
+				webRequest.getDescription(true));
+		return new ResponseEntity<>(applicationStatus, httpStatus);
+	}
+	
+	@ExceptionHandler(value = {InvalidEmployeeIdException.class})
+	public ResponseEntity<?> handleInvalidEmployeeIdException(InvalidEmployeeIdException ex, WebRequest webRequest){
+		
+		HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+		
+		ApplicationStatus applicationStatus = new ApplicationStatus(
+				ex.getMessage(),
+				ex.getCause(),
+				httpStatus,
+				ZonedDateTime.now(ZoneId.of("Z")),
+				webRequest.getDescription(false));
+		return new ResponseEntity<>(applicationStatus, httpStatus);
+	}
+	
+	@ExceptionHandler(value = {EmployeeNotFoundException.class})
+	public ResponseEntity<?> handleEmployeeNotFoundException(EmployeeNotFoundException ex, WebRequest webRequest){
+		
+		HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+		ApplicationStatus applicationStatus = new ApplicationStatus(
+				ex.getMessage(),
+				ex.getCause(),
+				httpStatus,
+				ZonedDateTime.now(ZoneId.of("Z")),
+				webRequest.getDescription(false)
+				);
+		return new ResponseEntity<>(applicationStatus,httpStatus);
 	}
 
 }
