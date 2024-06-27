79d26025c6c212f8307778da1ad9030c6f50d6d9
durga.maharjan.dev <durga.maharjan.dev@gmail.com>
2024-06-27 18:15:15 -0400

    EmailAlreadyExistsException updated

--- src/main/java/maharjanworks/exception/EmailAlreadyExistsException.java
+++ src/main/java/maharjanworks/exception/EmailAlreadyExistsException.java
@@ -4,7 +4,11 @@ public class EmailAlreadyExistsException extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
 
-	public EmailAlreadyExistsException(String email) {
-		super(email);
+	public EmailAlreadyExistsException(String message) {
+		super(message);
+	}
+	
+	public EmailAlreadyExistsException(String message, Throwable cause) {
+		super(message, cause);
 	}
 }
